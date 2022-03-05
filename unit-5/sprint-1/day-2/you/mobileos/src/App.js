1d69cf93ca42f6adf946274a52e55f5988374acb
sheeluofficial <sheeluofficial@gmail.com>
2022-03-05 22:07:47 +0530

    react intro

---
+++ unit-5/sprint-1/day-2/you/mobileos/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let title1 = "Mobile Operating System"
+  let title2 = "Mobile manufacturer"
+  let arr1 = ["Android","Blackberry","Iphone","Window Phone"]
+  let arr2 = ["Samsung","HTC","Micromax","Apple"]
+
+  return (
+    <div className="App">
+    <ListCreator title={title1} arr={arr1}/>
+    <ListCreator title={title2} arr={arr2}/>
+    </div>
+  );
+}
+
+function ListCreator({title,arr}) {
+  return (
+    <div>
+      <h1> {title} </h1>
+      <ul>
+        
+      {arr.map((item)=> <li> {item}</li> )}
+      </ul>
+    </div>
+  )
+}
+
+export default App;
